dba47a039f304c5c12723fbed31c1001cbb1f151
Artur Chilingaryan <1995.chilingaryan@gmail.com>
2020-11-20 23:33:20 +0400

    fix

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -4,7 +4,9 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
-	<meta name=”robots” content="index, follow">
+	<meta name="robots" content="index, follow">
+	<meta property="fb:app_id" content="808625669696768">
+	<meta property="fb:pages" content="268599243671295" />
 
 	@yield('meta')
 
