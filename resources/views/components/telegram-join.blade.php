1b58d6f95d566b37b85e54b606665656a53b9fb4
Artur Chilingaryan <1995.chilingaryan@gmail.com>
2020-11-24 03:11:01 +0400

    fix

--- resources/views/components/telegram-join.blade.php
+++ resources/views/components/telegram-join.blade.php
@@ -1,17 +1,19 @@
 <div class="telegram-join-page">
-	<div class="position-relative" onclick="clickTelegramLink()">
-		<div class="position-absolute circular-effect">
-			<div class="icon-container text-center">
-				<i class="fa fa-telegram-plane"></i>
-			</div>
-		</div>
-		<div class="join-text">
-			<div class="content-join-telegram">
-				<div>
-					{!! trans('main.telegram-popup-content') !!}
+	<div class="position-relative">
+		<a href="https://t.me/infoonenews" class="d-block" target="_blank" onclick="clickTelegramLink()">
+			<div class="position-absolute circular-effect">
+				<div class="icon-container text-center">
+					<i class="fa fa-telegram-plane"></i>
 				</div>
 			</div>
-		</div>
+			<div class="join-text">
+				<div class="content-join-telegram">
+					<div>
+						{!! trans('main.telegram-popup-content') !!}
+					</div>
+				</div>
+			</div>
+		</a>
 	</div>
 </div>
 
