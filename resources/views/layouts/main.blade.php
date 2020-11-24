4fda01de1e43a98d6ebe04b01a31f9c2a8acd73e
Artur Chilingaryan <1995.chilingaryan@gmail.com>
2020-11-24 18:25:31 +0400

    fix

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -86,7 +86,7 @@
 
 	<link rel="stylesheet" href="{{ asset('/css/bootstrap.min.css') }}">
 	<link rel="stylesheet" href="{{ asset('/css/all.min.css') }}">
-	<link rel="stylesheet" href="{{ asset('/css/styles.css?v=1.0.7') }}">
+	<link rel="stylesheet" href="{{ asset('/css/styles.css?v=1.0.8') }}">
 
 	@yield('scripts')
 </body>
