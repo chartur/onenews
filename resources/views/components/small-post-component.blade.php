01e832cb2218d3592b99ff0ddcc89ada170cd526
Artur Chilingaryan <1995.chilingaryan@gmail.com>
2020-11-24 17:56:53 +0400

    fix

--- resources/views/components/small-post-component.blade.php
+++ resources/views/components/small-post-component.blade.php
@@ -1,12 +1,12 @@
 <a href="{{ createPostLink($post->id) }}" class="text-decoration-none">
 	<div class="small-post-container">
 		<div class="mr-2 float-left small-post-image-container">
-			<img class="small-post-image" src="{{ url($post->image) }}">
+			<img class="small-post-image" src="{{ url(str_replace('/upload/', '/thumbs/', $post->image)) }}">
 		</div>
 		<div class="small-post-title-content">
 			<h4 class="small-post-title">{{ getAttributeByLang($post, 'title') }}</h4>
 		</div>
-		<div class="small-post-attributes text-right order-sm-0">
+		<div class="small-post-attributes text-center order-sm-0">
 			<span class="mr-2 main-color">
 				<i class="fa fa-calendar"></i>
 				{{ $post->created_at->formatLocalized('%d %b, %Y %H:%M') }}
