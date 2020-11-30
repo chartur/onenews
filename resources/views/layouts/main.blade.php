18386af2d8134e458f6de2476d51263ec9a886a9
Artur Chilingaryan <1995.chilingaryan@gmail.com>
2020-12-01 02:36:12 +0400

    popover

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -87,7 +87,7 @@
 
 	<link rel="stylesheet" href="{{ asset('/css/bootstrap.min.css') }}">
 	<link rel="stylesheet" href="{{ asset('/css/all.min.css') }}">
-	<link rel="stylesheet" href="{{ asset('/css/styles.css?v=1.1.9') }}">
+	<link rel="stylesheet" href="{{ asset('/css/styles.css?v=1.2.0') }}">
 
 	@yield('scripts')
 </body>
