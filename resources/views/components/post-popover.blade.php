9569f53d3cf5cd4a1793cca779c9ca4f85dcb1c4
Artur Chilingaryan <1995.chilingaryan@gmail.com>
2020-12-01 02:51:50 +0400

    popover

--- resources/views/components/post-popover.blade.php
+++ resources/views/components/post-popover.blade.php
@@ -1,6 +1,6 @@
 <div>
 	<iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fonenews.am%2F&tabs&width=340&height=130&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId=182306942842208" width="340" height="130" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe>
-	<div class="mt-4">
+	<div class="mt-5">
 		<button class="btn btn-block close-popover-modal-button" onclick="closePopover()">{{ trans('main.close') }}</button>
 	</div>
 </div>
