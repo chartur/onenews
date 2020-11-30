9079caa9009f0718877850c4523c6f382acb3da0
Artur Chilingaryan <1995.chilingaryan@gmail.com>
2020-12-01 00:23:25 +0400

    fix

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -87,7 +87,7 @@
 
 	<link rel="stylesheet" href="{{ asset('/css/bootstrap.min.css') }}">
 	<link rel="stylesheet" href="{{ asset('/css/all.min.css') }}">
-	<link rel="stylesheet" href="{{ asset('/css/styles.css?v=1.1.7') }}">
+	<link rel="stylesheet" href="{{ asset('/css/styles.css?v=1.1.8') }}">
 
 	@yield('scripts')
 </body>
