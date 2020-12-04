88d3f03c792e32cdde985701f80cdee685d4843a
Artur Chilingaryan <1995.chilingaryan@gmail.com>
2020-12-04 19:39:18 +0400

    fix

--- resources/views/post-page.blade.php
+++ resources/views/post-page.blade.php
@@ -97,7 +97,7 @@
 							<div class="post-image-container">
 								<img src="{{ $main_post->image }}" alt="{{ getAttributeByLang($main_post, $main_post->description ? 'description' : 'title') }}">
 								@if($ads->count())
-								<div class="mt-2 mb-2" style="width: 500px; max-width: 100%">
+								<div class="mt-2 mb-2 text-center">
 									{!! $ads->toArray()[1]['content'] !!}
 								</div>
 								@endif
@@ -224,7 +224,7 @@
 
 			clickTimer = setTimeout(function () {
 				$.fancybox.close();
-			}, 1000)
+			}, 1200)
 		}
 
 		@if(false)
