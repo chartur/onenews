bd7fa8b1742168fd761e8d5e0af82d3d2ef153d1
Artur Chilingaryan <1995.chilingaryan@gmail.com>
2021-09-12 01:31:52 +0400

    parse news am style

--- app/Http/Controllers/Admin/FacebookController.php
+++ app/Http/Controllers/Admin/FacebookController.php
@@ -9,6 +9,7 @@ use App\Parsers\BlogNewsParser;
 use App\Parsers\IravabanNetParser;
 use App\Parsers\LragirParser;
 use App\Parsers\NewsAmParser;
+use App\Parsers\NewsAmStyle;
 use App\Parsers\ShamshyanParser;
 
 class FacebookController extends Controller
@@ -18,7 +19,8 @@ class FacebookController extends Controller
 //        $parser = new NewsAmParser('https://news.am/arm/news/662305.html');
 //        $parser = new BlogNewsParser('https://www.blognews.am/arm/news/772053/arcakhy-chenq-korcrel-korcrel-enq-arcakhi-mi-masy-ory-tshnamin-stacel-e-gortsarqi-u-nviratvutyan-hetevanqov.html');
 //        $parser = new ShamshyanParser('https://shamshyan.com/hy/article/2021/09/11/1195832/');
-        $parser = new IravabanNetParser('https://iravaban.net/349165.html');
+//        $parser = new IravabanNetParser('https://iravaban.net/349165.html');
+        $parser = new NewsAmStyle('https://style.news.am/arm/news/82609/reshetovan-ordun-handznel-e-timatiin.html');
 
         return response()->json($parser->getPostData());
     }
